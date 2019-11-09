97f03786bf3999b946add8b8ad5bb2a5fb9ee47f
Chris Karvouniaris <christos.karvouniaris247@gmail.com>
2019-11-09 20:15:10 +0000

    add code in files types: .md remove code in files types:

--- content.py
+++ content.py
@@ -4,27 +4,36 @@ import os
 import json
 from datetime import datetime
 
-bitbucket_folder_path = "/Users/chriskarvouniaris/workspace_local/Bitbucket/"
+# bitbucket_folder_path = "/Users/chriskarvouniaris/workspace_local/Bitbucket/"
+bitbucket_folder_path = "/Users/chriskarvouniaris/workspace_local/bitbucket_to_see/"
 repo_relative_foler_paths = os.listdir(bitbucket_folder_path)
 repos = [git.Repo(os.path.join(bitbucket_folder_path, repo_relative_path)) for repo_relative_path in repo_relative_foler_paths]
+emails = [
+    'christos.karvouniaris247@gmail.com',
+    'c.karvouniaris@trebbble.co',
+    'christos.karvouniaris@lightsourcelabs.com'
+]
 
 last_import_ts_file_path = "last_import_ts.json"
 with open(last_import_ts_file_path, 'r') as f:
     json_data = json.load(f)
 
-if 'last_commit_git_import_ts' in json_data:
-    LAST_COMMIT_GIT_IMPORT_TS = int(json_data["last_commit_git_import_ts"])
+
 
 
 mock_repo = git.Repo(".", search_parent_directories=True)
 importer = Importer(repos, mock_repo)
-importer.set_author(['christos.karvouniaris247@gmail.com', 'c.karvouniaris@trebbble.co'])
-if LAST_COMMIT_GIT_IMPORT_TS:
-    importer.set_ignore_before_date(LAST_COMMIT_GIT_IMPORT_TS - 100000)
+importer.set_author(emails)
+
+if 'last_commit_git_import_ts' in json_data:
+    LAST_COMMIT_GIT_IMPORT_TS = int(json_data["last_commit_git_import_ts"])
+    importer.set_ignore_before_date(LAST_COMMIT_GIT_IMPORT_TS)
+
+
 importer.import_repository()
 
 with open(last_import_ts_file_path, "w") as outfile:
     body = {
         "last_commit_git_import_ts" : datetime.utcnow().strftime('%s')
     }
-print("vsltf")
+    json.dump(body, outfile)
