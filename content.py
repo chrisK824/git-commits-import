f2da6a761b40ba96d46a5db715e33eecbf9ab0da
chrisK824 <christos.karvouniaris247@gmail.com>
2023-11-24 15:08:34 +0000

    add code in files types: .py remove code in files types: .py

--- content.py
+++ content.py
@@ -26,4 +26,4 @@ importer.set_author(EMAILS)
 if LAST_IMPORTED_COMMIT_TIMESTAMP >= 0:
     importer.set_ignore_before_date(LAST_IMPORTED_COMMIT_TIMESTAMP)
 
-importer.import_repository(new_author=NEW_AUTHOR)
+print("qdhhc")
