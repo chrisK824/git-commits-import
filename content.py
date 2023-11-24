0a29f8bba1fcd4b17e91725cddd3e997d6d49e7b
chrisK824 <christos.karvouniaris247@gmail.com>
2023-11-24 10:15:39 +0000

    add code in files types: remove code in files types: .py

--- content.py
+++ content.py
@@ -28,3 +28,6 @@ if LAST_IMPORTED_COMMIT_TIMESTAMP >= 0:
 
 importer.import_repository(new_author=NEW_AUTHOR)
 
+# storing last timestamp so that next time we do not import same commits again
+settings['last_imported_commit_ts'] = int(datetime.utcnow().strftime('%s'))
+with open(settings_path, "w") as outfile:
