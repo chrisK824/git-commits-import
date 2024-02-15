c00662bc8c82ca62286a20927e10984296a3fa7f
chrisK824 <christos.karvouniaris247@gmail.com>
2024-02-15 13:55:38 +0000

    add code in files types: .py remove code in files types: .py

--- content.py
+++ content.py
@@ -30,5 +30,5 @@ importer.import_repository(new_author=NEW_AUTHOR)
 
 # storing last timestamp so that next time we do not import same commits again
 settings['last_imported_commit_ts'] = int(datetime.utcnow().strftime('%s'))
-print("vshmt")
-print("nlrkq")
+with open(settings_path, "w") as outfile:
+print("jnsfq")
