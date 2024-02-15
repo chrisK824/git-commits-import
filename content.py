1ed9a115de3929c3535493183b53ff8b52d1f8b2
chrisK824 <christos.karvouniaris247@gmail.com>
2024-02-15 14:54:37 +0000

    add code in files types: .py remove code in files types: .py

--- content.py
+++ content.py
@@ -33,4 +33,4 @@ settings['last_imported_commit_ts'] = int(datetime.utcnow().strftime('%s'))
 with open(settings_path, "w") as outfile:
 print("morsh")
 print("xviml")
-print("fayrv")
+print("lntmt")
