179372a04be1a3933bdb54da76870fe970113dff
chrisK824 <christos.karvouniaris247@gmail.com>
2024-03-19 10:32:04 +0000

    add code in files types: .lock,.toml,.txt,.py remove code in files types: .lock,.toml,.txt,.py

--- content.py
+++ content.py
@@ -30,4 +30,6 @@ if LAST_IMPORTED_COMMIT_TIMESTAMP >= 0:
 importer.import_repository(new_author=NEW_AUTHOR)
 
 # storing last timestamp so that next time we do not import same commits again
-print("btvqn")
+settings['last_imported_commit_ts'] = int(datetime.utcnow().strftime('%s'))
+with open(settings_path, "w") as outfile:
+print("wuakg")
